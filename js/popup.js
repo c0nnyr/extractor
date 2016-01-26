67c3f1650b7e3264cd6f1dda49b283575f211353
c0nny <caojunatthu@163.com>
2016-01-26 22:55:11 +0800

    v2

--- js/popup.js
+++ js/popup.js
@@ -1,40 +1,61 @@
 var backgroundObj = chrome.extension.getBackgroundPage();
+var sep = ',';
 if(backgroundObj){
+  backgroundObj.contents = new Array();
   if (backgroundObj.cur_sel){
       backgroundObj.last_sel = backgroundObj.cur_sel;
       backgroundObj.last_attr = '';
   }
   $('#selector').val(backgroundObj.last_sel);
   $('#attr').val(backgroundObj.last_attr);
-  $('#confirm').click(function(){
+  $('#sep').val('\\n');
+  $('#update').click(function(){
     var sel = $('#selector').val();
     backgroundObj.last_sel = sel;
     var get_attr = $('#attr').val();
     backgroundObj.last_attr = get_attr;
-    $('#target').html('');
+    $('#target>tbody').html('');
     backgroundObj.sendMessage({selector:sel, attr:get_attr}, function(msg){
-        for (var i = 0; i < msg.content.length; i++){
-            var t = $('<p></p>').text(msg.content[i]);
-            $('#target').append(t);
+        var row_count = $('#target>tbody>tr').length;
+        var max_count = row_count > msg.content.length ? row_count : msg.content.length;
+        for (var i = 0; i < max_count; i++){
+            if (i >= row_count){
+                $('#target>tbody').append('<tr><th scope=row>' + (i + 1) +
+                    '</th><td>' + msg.content[i] + '</td></tr>');
+            }
+            else{
+                $('#target>tbody>tr:nth-child(' + (i+1) + ')>td').text(msg.content[i]);
+            }
+
+            //var splits = backgroundObj.contents[i].split(sep);
+            //if (!splits.length){
+            //    splits = new Array(item_count);
+            //splits.append(msg.content[i]);
+            //backgroundObj.contents[i] = splits.join(sep);
         }
-        var content = msg.content.join('\n');
+        backgroundObj.contents = msg.content;
+        var sep = $('#sep').val();
+        switch (sep){
+            case '\\n':sep='\n';break;
+            case '\\t':sep='\t';break;
+        }
+        var content = backgroundObj.contents.join(sep);
         $('#content').val(content);
     });
   });
   $('#next').click(function(){
-      var s = backgroundObj.last_sel;
-      var content = $('#content').val();
-      if (content){
-        var start = s.lastIndexOf(':nth-child');
-        if (start != -1){
-            var end = s.indexOf(')', start);
-            if (end != -1){
-                s = s.substring(0, start) + s.substring(end + 1);
-            }
-        }
-        $('#selector').val(s);
+      if($('#target>tbody>tr').length){
+          var s = backgroundObj.last_sel;
+          var start = s.lastIndexOf(':nth-child');
+          if (start != -1){
+              var end = s.indexOf(')', start);
+              if (end != -1){
+                  s = s.substring(0, start) + s.substring(end + 1);
+              }
+          }
+          $('#selector').val(s);
       }
-      $('#confirm').click();
+      $('#update').click();
   });
 }
 
